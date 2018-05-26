245860395d60fdc8e244a6fd48947a120ee03a46
sander.puts <putssander@gmail.com>
2018-05-26 22:46:41 +0200

    test extended to read write csv

--- tests/umlsradlex_test.py
+++ tests/umlsradlex_test.py
@@ -1,14 +1,16 @@
 import unittest
 from umlsradlex.umlsradlex import UMLSRadlex
 
-radlex_csv = ""
-output_dir = ""
+radlex_csv = "./../resources/RADLEX.csv"
+output_dir = "./../out"
+config_file = "'./../config.json"
 
 
 class TestUMLSIntegration(unittest.TestCase):
 
     def test_upper(self):
-        umls_radlex = UMLSRadlex(radlex_csv, output_dir)
+        umls_radlex = UMLSRadlex(radlex_csv, output_dir, config_file)
+        umls_radlex.integrate()
 
 
 if __name__ == '__main__':
--- umlsradlex/umlsradlex.py
+++ umlsradlex/umlsradlex.py
@@ -1,4 +1,6 @@
 import csv
+import configparser
+import json
 
 
 class UMLSRadlex(object):
@@ -12,17 +14,26 @@ class UMLSRadlex(object):
     radlex_csv = ""
     output_dir = ""
 
-    def __init__(self, radlex_csv, output_dir):
+    def __init__(self, radlex_csv, output_dir, config_file):
         self.radlex_csv = radlex_csv
         self.output_dir = output_dir
+        with open(config_file, 'r') as f:
+            self.config = json.load(f)
+
+    def set_indices(self):
+        print('set config')
+
 
     def integrate(self):
         with open(self.radlex_csv, "rt", encoding='utf8') as csvfile, \
-                open(self.output_dir + '/out.csv', "wb") as outfile:
+                open(self.output_dir + '/out.csv', "w", encoding='utf8', newline="\n") as outfile:
 
                 reader = csv.reader(csvfile, delimiter=',', quotechar='|')
                 next(reader, None)  # skip the headers
-                writer = csv.writer(outfile)
+                writer = csv.writer(outfile, delimiter='|')
                 for row in reader:
+
                         printrow = ', '.join(row)
                         print(printrow)
+                        writer.writerow(row)
+
